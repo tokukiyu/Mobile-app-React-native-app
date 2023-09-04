641e66c90ac33f72e8a242cd373aaa77f5d8a4cb
tokukiyu <tokkiyuam@gmail.com>
2023-09-04 10:54:45 +0300

    update

--- components/LittleLemonHeader.js
+++ components/LittleLemonHeader.js
@@ -1,12 +1,17 @@
-import * as React from 'react';  
 import { View, Text } from 'react-native';
-    
+
 export default function LittleLemonHeader() {
-    return (
-      <View style={{ flex: 0.2, backgroundColor: '#F4CE14' }}>
-        <Text style={{ padding: 40, fontSize: 30, color: 'black' }}>
-          Welcome to Little Lemon
-        </Text>
-      </View>
-    );
+  return (
+    <View style={{ flex: 0.15, backgroundColor: '#F4CE14' }}>
+      <Text
+        style={{
+          padding: 40,
+          fontSize: 30,
+          color: 'black',
+          textAlign: 'center',
+        }}>
+        Little Lemon
+      </Text>
+    </View>
+  );
 }
