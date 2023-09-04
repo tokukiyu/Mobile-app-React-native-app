125c3b61c02f95d91ec67dd450e4bca35834f2df
tokukiyu <tokkiyuam@gmail.com>
2023-09-04 11:16:47 +0300

    update

--- App.js
+++ App.js
@@ -1,16 +1,20 @@
-import * as React from 'react';
-import { View } from 'react-native';
+import * as React from "react";
+import { View, Text } from 'react-native';
 
- // Import a local component here
- import LittleLemonHeader from './components/LittleLemonHeader';
- export default function App() {
-   return (
-     <View
-       style={{
-         flex: 1,
-         backgroundColor: '#495E57',
-       }}>
-       <LittleLemonHeader />
-     </View>
-   );
- }
+// Import a local component here
+import LittleLemonHeader from "./components/LittleLemonHeader";
+
+export default function App() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: "#495E57",
+        justifyContent: "center", // Center content vertically
+        alignItems: "center",     // Center content horizontally
+      }}
+    >
+     <LittleLemonHeader />
+    </View>
+  );
+}
