428e9bffb0663e4b243f97402461eba7a6536f5a
tokukiyu <tokkiyuam@gmail.com>
2023-09-05 09:59:19 +0300

    update 3

--- components/LittleLemonHeader.js
+++ components/LittleLemonHeader.js
@@ -1,12 +1,28 @@
-import * as React from "react";
-import { View, Text } from "react-native";
+import * as React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 
 export default function LittleLemonHeader() {
   return (
-    <View style={{ backgroundColor: "#F4CE14" }}>
-      <Text style={{ padding: 14,textAlign:"center", fontSize: 30, color: "black" }}>
-        Little Lemon
+    <View style={headerStyles.container}>
+      <Text style={headerStyles.headerText}>
+	  Welcome to
+        <Text style={headerStyles.innerText}> Little Lemon</Text>
       </Text>
     </View>
   );
 }
+
+const headerStyles = StyleSheet.create({
+  container: {
+    backgroundColor: '#F4CE14',
+  },
+  headerText: {
+    padding: 40,
+    fontSize: 30,
+    color: 'black',
+    textAlign: 'center',
+  },
+  innerText: {
+    fontWeight: 'bold',
+  },
+});
