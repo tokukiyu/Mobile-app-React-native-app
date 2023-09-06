5fd5209cacfa0ad9088493c716a483828cf1bdc8
tokukiyu <tokkiyuam@gmail.com>
2023-09-06 11:36:52 +0300

    cor

--- components/MenuItems.js
+++ components/MenuItems.js
@@ -57,7 +57,7 @@ const menuStyles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between',
   },
-  itemText: { chrom
+  itemText: { 
     color: '#F4CE14',
     fontSize: 20,
   },
