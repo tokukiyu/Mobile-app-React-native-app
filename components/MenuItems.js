99d2684eacb22be2494a987bb6119a47796ab9a7
tokukiyu <tokkiyuam@gmail.com>
2023-09-06 12:38:15 +0300

    sectionList exercise

--- components/MenuItems.js
+++ components/MenuItems.js
@@ -6,33 +6,43 @@ const menuItemsToDisplay = [
   {
     title: 'Appetizers',
     data: [
-      'Hummus',
-      'Moutabal',
-      'Falafel',
-      'Marinated Olives',
-      'Kofta',
-      'Eggplant Salad',
+      { name: 'Hummus', price: '$5.00' },
+      { name: 'Moutabal', price: '$5.00' },
+      { name: 'Falafel', price: '$7.50' },
+      { name: 'Marinated Olives', price: '$5.00' },
+      { name: 'Kofta', price: '$5.00' },
+      { name: 'Eggplant Salad', price: '$8.50' },
     ],
   },
   {
     title: 'Main Dishes',
-    data: ['Lentil Burger', 'Smoked Salmon', 'Kofta Burger', 'Turkish Kebab'],
+    data: [
+      { name: 'Lentil Burger', price: '$10.00' },
+      { name: 'Smoked Salmon', price: '$14.00' },
+      { name: 'Kofta Burger', price: '$11.00' },
+      { name: 'Turkish Kebab', price: '$15.50' },
+    ],
   },
   {
     title: 'Sides',
     data: [
-      'Fries',
-      'Buttered Rice',
-      'Bread Sticks',
-      'Pita Pocket',
-      'Lentil Soup',
-      'Greek Salad',
-      'Rice Pilaf',
+      { name: 'Fries', price: '$3.00', id: '11K' },
+      { name: 'Buttered Rice', price: '$3.00' },
+      { name: 'Bread Sticks', price: '$3.00' },
+      { name: 'Pita Pocket', price: '$3.00' },
+      { name: 'Lentil Soup', price: '$3.75' },
+      { name: 'Greek Salad', price: '$6.00' },
+      { name: 'Rice Pilaf', price: '$4.00' },
     ],
   },
   {
     title: 'Desserts',
-    data: ['Baklava', 'Tartufo', 'Tiramisu', 'Panna Cotta'],
+    data: [
+      { name: 'Baklava', price: '$3.00' },
+      { name: 'Tartufo', price: '$3.00' },
+      { name: 'Tiramisu', price: '$5.00' },
+      { name: 'Panna Cotta', price: '$5.00' },
+    ],
   },
 ];
 
@@ -43,62 +53,46 @@ const Item = ({ name, price }) => (
   </View>
 );
 
-const Separator = () => <View style={menuStyles.separator} />;
-
-const Footer = () => (
-  <Text style={menuStyles.footerText}>
-    All Rights Reserved by Little Lemon 2022
-  </Text>
-);
-
 const MenuItems = () => {
-  const renderItem = ({ item }) => <Item name={item} />;
+  const renderItem = ({ item }) => <Item name={item.name} price={item.price} />;
 
   const renderSectionHeader = ({ section: { title } }) => (
-    <Text style={menuStyles.sectionHeader}>{title} </Text>
+    <View style={menuStyles.headerStyle}>
+      <Text style={menuStyles.sectionHeader}>{title}</Text>
+    </View>
   );
 
   return (
     <View style={menuStyles.container}>
       <SectionList
-        keyExtractor={(item, index) => item + index}
         sections={menuItemsToDisplay}
+        keyExtractor={(item, index) => item + index}
         renderItem={renderItem}
-        renderSectionHeader={renderSectionHeader}
-        ListFooterComponent={Footer}
-        ItemSeparatorComponent={Separator}></SectionList>
+        renderSectionHeader={renderSectionHeader}></SectionList>
     </View>
   );
 };
 
-// Add styles to the component
 const menuStyles = StyleSheet.create({
   container: {
-    flex: 0.95,
+    flex: 1,
   },
   innerContainer: {
     paddingHorizontal: 40,
     paddingVertical: 20,
-    backgroundColor: '#333333',
-  },
-  sectionHeader: {
-    backgroundColor: '#fbdabb',
-    color: '#333333',
-    fontSize: 34,
-    flexWrap: 'wrap',
-    textAlign: 'center',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
   },
   itemText: {
     color: '#F4CE14',
-    fontSize: 32,
-  },
-  separator: {
-    borderBottomWidth: 1,
-    borderColor: '#EDEFEE',
-  },
-  footerText: {
-    color: '#EDEFEE',
     fontSize: 20,
+  },
+  headerStyle: {
+    backgroundColor: '#F4CE14',
+  },
+  sectionHeader: {
+    color: 'black',
+    fontSize: 26,
     flexWrap: 'wrap',
     textAlign: 'center',
   },
