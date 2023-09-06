00cd90a9bb0f2d4757b2e522e3e026fad0151c33
tokukiyu <tokkiyuam@gmail.com>
2023-09-06 16:11:06 +0300

    feedback

--- App.js
+++ App.js
@@ -4,13 +4,14 @@ import { View, StyleSheet } from 'react-native';
 import LittleLemonHeader from './components/LittleLemonHeader';
 import LittleLemonFooter from './components/LittleLemonFooter';
 import MenuItems from './components/MenuItems';
+import FeedbackForm from './components/FeedBackForm';
 
 export default function App() {
   return (
     <>
       <View style={styles.container}>
         <LittleLemonHeader />
-        <MenuItems />
+        <FeedbackForm />
       </View>
       <View style={styles.footerContainer}>
         <LittleLemonFooter />
