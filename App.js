17d8d5a09e559cd7dbf5a0818e891b88b9c0284a
tokukiyu <tokkiyuam@gmail.com>
2023-09-06 11:35:59 +0300

    upt

--- App.js
+++ App.js
@@ -2,20 +2,27 @@ import * as React from 'react';
 import { View, StyleSheet } from 'react-native';
 
 import LittleLemonHeader from './components/LittleLemonHeader';
+import LittleLemonFooter from './components/LittleLemonFooter';
 import MenuItems from './components/MenuItems';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <LittleLemonHeader />
-      <MenuItems />
-    </View>
+    <>
+      <View style={styles.container}>
+        <LittleLemonHeader />
+        <MenuItems />
+      </View>
+      <View style={styles.footerContainer}>
+        <LittleLemonFooter />
+      </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#495E57',
+    backgroundColor: '#333333',
   },
+  footerContainer: { backgroundColor: '#333333' },
 });
