d6ec9e63c0c11771ff6f2bd58783c248bc93ccc4
tokukiyu <tokkiyuam@gmail.com>
2023-09-07 09:51:46 +0300

    image 2

--- Welcome.js
+++ Welcome.js
@@ -1,22 +1,61 @@
 import React from 'react';
-import { View, Image, StyleSheet, Text } from 'react-native';
+import { ScrollView, Image, StyleSheet, Text } from 'react-native';
 
 const Welcome = () => {
   return (
-    <View style={styles.container}>
-      <Image style={styles.logo} source={require('img/littleLemonLogo.png')} />
+    <ScrollView style={styles.container}>
+      <Image
+        style={styles.logo}
+        source={require('../img/littleLemonLogo.png')}
+        resizeMode="center"
+        accessible={true}
+        accessibilityLabel={'Little Lemon Logo'}
+      />
+
       <Text style={styles.title}>
         Little Lemon, your local Mediterranean Bistro
       </Text>
-    </View>
+      <Image
+        style={styles.image}
+        source={require('../img/Picture1.png')}
+        resizeMode="cover"
+        accessible={true}
+        accessibilityLabel={'Little Lemon Logo'}
+      />
+      <Image
+        style={styles.image}
+        source={require('../img/Picture2.png')}
+        resizeMode="cover"
+        accessible={true}
+        accessibilityLabel={'Little Lemon Logo'}
+      />
+      <Image
+        style={styles.image}
+        source={require('../img/Picture3.png')}
+        resizeMode="cover"
+        accessible={true}
+        accessibilityLabel={'Little Lemon Logo'}
+      />
+      <Image
+        style={styles.image}
+        source={require('../img/Picture4.png')}
+        resizeMode="cover"
+        accessible={true}
+        accessibilityLabel={'Little Lemon Logo'}
+      />
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
   logo: {
-     height: 100,
-     width: 300,
-     resizeMode: 'contain',
+    height: 100,
+    width: 300,
+  },
+  image: {
+    width: 350,
+    height: 250,
+    borderRadius: 10,
   },
   container: {
     flex: 1,
