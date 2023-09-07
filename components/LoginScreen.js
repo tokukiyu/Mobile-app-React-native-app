1511bc2bb13cd84edccccf7112820b1a4cf19fbc
tokukiyu <tokkiyuam@gmail.com>
2023-09-07 10:56:25 +0300

    button more

--- components/LoginScreen.js
+++ components/LoginScreen.js
@@ -1,39 +1,45 @@
-import React, { useState } from "react";
-import { ScrollView, Text, StyleSheet, TextInput,Alert } from "react-native";
+import React, { useState } from 'react';
+import {
+  ScrollView,
+  Text,
+  StyleSheet,
+  TextInput,
+  Pressable,
+} from 'react-native';
 
 export default function LoginScreen() {
-  const [email, onChangeEmail] = useState("");
-  const [password, onChangePassword] = useState("");
+  const [email, onChangeEmail] = useState('');
+  const [password, onChangePassword] = useState('');
+  const [loggedIn, onLogin] = useState(false);
 
-  const [firstName, onChangeFirstName] = useState("");
   return (
     <ScrollView style={styles.container}>
       <Text style={styles.headerText}>Welcome to Little Lemon</Text>
-      <Text style={styles.regularText}>Login to continue </Text>
-      <TextInput
-        style={styles.inputBox}
-        value={email}
-        onChangeText={onChangeEmail}
-        placeholder={"email"}
-        keyboardType={"email-address"}
-      />
-      <TextInput
-        style={styles.inputBox}
-        value={password}
-        onChangeText={onChangePassword}
-        placeholder={"password"}
-        keyboardType={"default"}
-        secureTextEntry={true}
-      /> 
-      <TextInput
-         style={styles.inputBox}
-        value={firstName}
-        onChangeText={onChangeFirstName}
-        placeholder={"First Name"} 
-        onFocus={() => {
-          Alert.alert("First name is focussed");
-        }}
-      />
+      {loggedIn && <Text style={styles.headerText}>You are logged in!</Text>}
+
+      {!loggedIn && (
+        <>
+          <Text style={styles.regularText}>Login to continue </Text>
+          <TextInput
+            style={styles.inputBox}
+            value={email}
+            onChangeText={onChangeEmail}
+            placeholder={'email'}
+            keyboardType={'email-address'}
+          />
+          <TextInput
+            style={styles.inputBox}
+            value={password}
+            onChangeText={onChangePassword}
+            placeholder={'password'}
+            keyboardType={'default'}
+            secureTextEntry={true}
+          />
+          <Pressable onPress={() => onLogin(!loggedIn)} style={styles.button}>
+            <Text style={styles.buttonText}>Log in</Text>
+          </Pressable>
+        </>
+      )}
     </ScrollView>
   );
 }
@@ -45,15 +51,15 @@ const styles = StyleSheet.create({
   headerText: {
     padding: 40,
     fontSize: 30,
-    color: "#EDEFEE",
-    textAlign: "center",
+    color: '#EDEFEE',
+    textAlign: 'center',
   },
   regularText: {
     fontSize: 24,
     padding: 20,
     marginVertical: 8,
-    color: "#EDEFEE",
-    textAlign: "center",
+    color: '#EDEFEE',
+    textAlign: 'center',
   },
   inputBox: {
     height: 40,
@@ -61,7 +67,23 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     padding: 10,
     fontSize: 16,
-    borderColor: "#EDEFEE", // Fix the color format
-    backgroundColor: "#EDEFEE",
+    borderColor: 'EDEFEE',
+    backgroundColor: '#EDEFEE',
+  },
+  button: {
+    fontSize: 22,
+    padding: 10,
+    marginVertical: 8,
+    margin: 100,
+    backgroundColor: '#EE9972',
+    borderColor: '#EE9972',
+    borderWidth: 2,
+    borderRadius: 50,
+  },
+  buttonText: {
+    color: 'black',
+    textAlign: 'center',
+    fontSize: 25,
   },
 });
+
