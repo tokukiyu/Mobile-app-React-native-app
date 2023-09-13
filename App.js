8c93f5d748805f97501ae541d1e0a9ab000df802
tokukiyu <tokkiyuam@gmail.com>
2023-09-13 17:11:40 +0300

    up

--- App.js
+++ App.js
@@ -1,23 +1,27 @@
-import * as React from 'react';
-import { View, StyleSheet } from 'react-native';
+import * as React from "react";
+import { View, StyleSheet } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
 
-import LittleLemonHeader from './components/LittleLemonHeader';
-import LittleLemonFooter from './components/LittleLemonFooter';
-import MenuItems from './components/MenuItems';
-import FeedbackForm from './components/FeedBackForm';
-import LoginScreen from './components/LoginScreen';
-import Welcome from './Welcome';
+import LittleLemonHeader from "./components/LittleLemonHeader";
+import LittleLemonFooter from "./components/LittleLemonFooter";
+import MenuItems from "./components/MenuItems";
+import FeedbackForm from "./components/FeedBackForm";
+import LoginScreen from "./components/LoginScreen";
+import Welcome from "./Welcome";
 
 export default function App() {
   return (
     <>
-      <View style={styles.container}>
-        <LittleLemonHeader />
-        <Welcome />
-      </View>
-      <View style={styles.footerContainer}>
-        <LittleLemonFooter />
-      </View>
+      <NavigationContainer>
+        {" "}
+        <View style={styles.container}>
+          <LittleLemonHeader />
+          <Welcome />
+        </View>
+        <View style={styles.footerContainer}>
+          <LittleLemonFooter />
+        </View>
+      </NavigationContainer>
     </>
   );
 }
@@ -25,7 +29,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#333333',
+    backgroundColor: "#333333",
   },
-  footerContainer: { backgroundColor: '#333333' },
+  footerContainer: { backgroundColor: "#333333" },
 });
