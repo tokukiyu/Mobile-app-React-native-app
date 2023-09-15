cc0d1eecf9f2b0a9ea3613748719a3d77acb6b28
tokukiyu <tokkiyuam@gmail.com>
2023-09-15 09:24:32 +0300

    update navigator

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import * as React from "react";
-import { View, StyleSheet } from "react-native";
+import { View, Text } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import LittleLemonHeader from "./components/LittleLemonHeader";
 import LittleLemonFooter from "./components/LittleLemonFooter";
@@ -8,22 +9,19 @@ import MenuItems from "./components/MenuItems";
 import FeedbackForm from "./components/FeedBackForm";
 import LoginScreen from "./components/LoginScreen";
 import Welcome from "./Welcome";
-
+const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <>
       <NavigationContainer>
-        {" "}
-        <View style={styles.container}>
-          <LittleLemonHeader />
-          <Welcome />
-        </View>
-        <View style={styles.footerContainer}>
-          <LittleLemonFooter />
-        </View>
+        <Stack.Navigator initialRouteName="Welcome">
+          <Stack.Screen name="Welcome" component={Welcome} />
+          <Stack.Screen name="Menu" component={MenuItems} />
+        </Stack.Navigator>
+       
       </NavigationContainer>
-    </> 
-  ); 
+    </>
+  );
 }
 
 const styles = StyleSheet.create({
