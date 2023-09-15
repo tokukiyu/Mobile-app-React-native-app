c20865bfd54441306287d219cf57623fe58c5500
tokukiyu <tokkiyuam@gmail.com>
2023-09-15 09:41:01 +0300

    update

--- App.js
+++ App.js
@@ -1,24 +1,29 @@
-import * as React from "react";
-import { View, Text } from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import * as React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 
-import LittleLemonHeader from "./components/LittleLemonHeader";
-import LittleLemonFooter from "./components/LittleLemonFooter";
-import MenuItems from "./components/MenuItems";
-import FeedbackForm from "./components/FeedBackForm";
-import LoginScreen from "./components/LoginScreen";
-import Welcome from "./Welcome";
+import LittleLemonHeader from './components/LittleLemonHeader';
+import LittleLemonFooter from './components/LittleLemonFooter';
+import Welcome from './Welcome';
+import LoginScreen from './LoginScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
 const Stack = createNativeStackNavigator();
+
 export default function App() {
   return (
     <>
       <NavigationContainer>
-        <Stack.Navigator initialRouteName="Welcome">
-          <Stack.Screen name="Welcome" component={Welcome} />
-          <Stack.Screen name="Menu" component={MenuItems} />
-        </Stack.Navigator>
-       
+        <View style={styles.container}>
+          <LittleLemonHeader />
+          <Stack.Navigator initialRouteName="Login">
+            <Stack.Screen name="Welcome" component={Welcome} />
+            <Stack.Screen name="Login" component={LoginScreen} />
+          </Stack.Navigator>
+        </View>
+        <View style={styles.footerContainer}>
+          <LittleLemonFooter />
+        </View>
       </NavigationContainer>
     </>
   );
@@ -27,7 +32,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#333333",
+    backgroundColor: '#333333',
   },
-  footerContainer: { backgroundColor: "#333333" },
+  footerContainer: { backgroundColor: '#333333' },
 });
+
