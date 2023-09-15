18160e5215372911fff91e1f3e1e93b411a63aa4
tokukiyu <tokkiyuam@gmail.com>
2023-09-15 19:27:37 +0300

    API request

--- App.js
+++ App.js
@@ -1,41 +1,80 @@
-import * as React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useEffect, useState } from "react";
+import {
+  ActivityIndicator,
+  FlatList,
+  Text,
+  View,
+  StyleSheet,
+  SafeAreaView,
+} from "react-native";
 
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+export default App = () => {
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
 
-import LittleLemonHeader from "./components/LittleLemonHeader";
-import LittleLemonFooter from "./components/LittleLemonFooter";
-import MenuItems from "./components/MenuItems";
-import Welcome from "./Welcome";
-import WelcomeScreen from "./welcomeScreen"
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+  const getMenu = async () => {
+    try {
+      const response = await fetch(
+        "https://raw.githubusercontent.com/Meta-Mobile-Developer-PC/Working-With-Data-API/main/menu-items-by-category.json"
+      );
+      const json = await response.json();
+      setData(json.menu);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
 
-const Stack = createNativeStackNavigator(); 
+  useEffect(() => {
+    getMenu();
+  }, []);
 
-export default function App() {
-  return (
-    <>
-      <NavigationContainer>
-        <View style={styles.container}>
-          <LittleLemonHeader />
-          <Stack.Navigator initialRouteName="Login">
-            <Stack.Screen name="Welcome" component={WelcomeScreen} />
-            <Stack.Screen name="menu" component={MenuItems} />
-          </Stack.Navigator>
-        </View>
-        <View style={styles.footerContainer}>
-          <LittleLemonFooter />
-        </View>
-      </NavigationContainer>
-    </>
+  const Item = ({ name, price }) => (
+    <View style={menuStyles.innerContainer}>
+      <Text style={menuStyles.itemText}>{name}</Text>
+      <Text style={menuStyles.itemText}>{"$" + price}</Text>
+    </View>
   );
-}
 
-const styles = StyleSheet.create({
+  const renderItem = ({ item }) => (
+    <Item name={item.title} price={item.price} />
+  );
+
+  return (
+    <SafeAreaView style={menuStyles.container}>
+      <Text style={menuStyles.headerText}>Little Lemon</Text>
+      {isLoading ? (
+        <ActivityIndicator />
+      ) : (
+        <FlatList
+          data={data}
+          keyExtractor={({ id }, index) => id}
+          renderItem={renderItem}
+        />
+      )}
+    </SafeAreaView>
+  );
+};
+
+const menuStyles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#333333",
   },
-  footerContainer: { backgroundColor: "#333333" },
+  innerContainer: {
+    paddingHorizontal: 40,
+    paddingVertical: 20,
+    backgroundColor: "#495E57",
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  itemText: {
+    color: "#F4CE14",
+    fontSize: 22,
+  },
+  headerText: {
+    color: "#495E57",
+    fontSize: 30,
+    textAlign: "center",
+  },
 });
