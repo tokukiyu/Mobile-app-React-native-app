8e1f87f1947f3ee5a756674a703acedfecb3e366
tokukiyu <tokkiyuam@gmail.com>
2023-09-17 11:28:13 +0300

    menu bar done

--- App.js
+++ App.js
@@ -6,11 +6,13 @@ import {
   View,
   StyleSheet,
   SafeAreaView,
+  TouchableOpacity,
 } from "react-native";
 
 export default App = () => {
   const [isLoading, setLoading] = useState(true);
   const [data, setData] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState("Appetizers"); // Default to "Appetizers"
 
   const getMenu = async () => {
     try {
@@ -30,6 +32,11 @@ export default App = () => {
     getMenu();
   }, []);
 
+  // Filter data based on selected category
+  const filteredData = data.filter(
+    (item) => item.category.title === selectedCategory
+  );
+
   const Item = ({ name, price }) => (
     <View style={menuStyles.innerContainer}>
       <Text style={menuStyles.itemText}>{name}</Text>
@@ -44,11 +51,45 @@ export default App = () => {
   return (
     <SafeAreaView style={menuStyles.container}>
       <Text style={menuStyles.headerText}>Little Lemon</Text>
+
+      {/* Category selection buttons */}
+      <View style={menuStyles.categoryButtons}>
+        <TouchableOpacity
+          style={[
+            menuStyles.categoryButton,
+            selectedCategory === "Appetizers" &&
+              menuStyles.selectedCategoryButton,
+          ]}
+          onPress={() => setSelectedCategory("Appetizers")}
+        >
+          <Text>Appetizers</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={[
+            menuStyles.categoryButton,
+            selectedCategory === "Salads" && menuStyles.selectedCategoryButton,
+          ]}
+          onPress={() => setSelectedCategory("Salads")}
+        >
+          <Text>Salads</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={[
+            menuStyles.categoryButton,
+            selectedCategory === "Beverages" &&
+              menuStyles.selectedCategoryButton,
+          ]}
+          onPress={() => setSelectedCategory("Beverages")}
+        >
+          <Text>Beverages</Text>
+        </TouchableOpacity>
+      </View>
+
       {isLoading ? (
         <ActivityIndicator />
       ) : (
         <FlatList
-          data={data}
+          data={filteredData} // Display filtered data
           keyExtractor={({ id }, index) => id}
           renderItem={renderItem}
         />
@@ -76,5 +117,24 @@ const menuStyles = StyleSheet.create({
     color: "#495E57",
     fontSize: 30,
     textAlign: "center",
+  },
+  categoryButtons: {
+    flexDirection: "row",
+    justifyContent: "center",
+    marginVertical: 10,
+  },
+  categoryButton: {
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    marginHorizontal: 5,
+    backgroundColor: "#495E57",
+    borderRadius: 5,
+  },
+  selectedCategoryButton: {
+    backgroundColor: "#F4CE14",
+    color: "#FFFFFF",
+  },
+  categoryButtonText: {
+    color: "#F4CE14",
   },
 });
