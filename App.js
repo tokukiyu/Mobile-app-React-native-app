0aa705c1208df0f1ace2f371e628e9732bc4c1f1
tokukiyu <tokkiyuam@gmail.com>
2023-09-17 10:01:47 +0300

    project

--- App.js
+++ App.js
@@ -1,74 +1,80 @@
-import React, { useState, useEffect } from 'react';
-import { Text, View, StyleSheet, SafeAreaView } from 'react-native';
+import React, { useEffect, useState } from "react";
+import {
+  ActivityIndicator,
+  FlatList,
+  Text,
+  View,
+  StyleSheet,
+  SafeAreaView,
+} from "react-native";
 
-export default function App() {
- const [myJSON, setJsonObj] = useState([]);
+export default App = () => {
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
 
- const convertStringToJson = () => {
-  const myStr = `[
-    {
-        "name": "Veggie Kabob",
-        "price": "$12",
-        "type": "Main Dish" 
-    },
-    { 
-        "name": "Greek Salad", 
-        "price": "$7",
-        "type": "Side  "
-    },
-    {
-        "name": "Black Tea",
-        "price": "$3",
-        "type": "Drink"
+  const getMenu = async () => {
+    try {
+      const response = await fetch(
+        "https://raw.githubusercontent.com/Meta-Mobile-Developer-PC/Working-With-Data-API/main/menu-items-by-category.json"
+      );
+      const json = await response.json();
+      setData(json.menu);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
     }
-]`;
+  };
 
+  useEffect(() => {
+    getMenu();
+  }, []);
 
-  const result = JSON.parse(myStr);
-  setJsonObj(result);
- };
+  const Item = ({ name, price }) => (
+    <View style={menuStyles.innerContainer}>
+      <Text style={menuStyles.itemText}>{name}</Text>
+      <Text style={menuStyles.itemText}>{"$" + price}</Text>
+    </View>
+  );
 
- useEffect(() => {
-  convertStringToJson();
- }, []);
+  const renderItem = ({ item }) => (
+    <Item name={item.title} price={item.price} />
+  );
 
- return (
-  <SafeAreaView style={styles.container}>
-   <Text style={styles.headerText}>Little Lemon</Text>
-   {myJSON &&
-    myJSON.map((menuItem) => {
-     return (
-      <View style={styles.innerContainer}>
-       <Text style={styles.itemText}>{'Menu Item: ' + menuItem.name}</Text>
-       <Text style={styles.itemText}>{'Price: ' + menuItem.price}</Text>
-       <Text style={styles.itemText}>{'Type: ' + menuItem.type}</Text>
-      </View>
-     );
-    })}
-  </SafeAreaView>
- );
-}
- 
+  return (
+    <SafeAreaView style={menuStyles.container}>
+      <Text style={menuStyles.headerText}>Little Lemon</Text>
+      {isLoading ? (
+        <ActivityIndicator />
+      ) : (
+        <FlatList
+          data={data}
+          keyExtractor={({ id }, index) => id}
+          renderItem={renderItem}
+        />
+      )}
+    </SafeAreaView>
+  );
+};
 
-const styles = StyleSheet.create({
- container: {
-  flex: 1,
-  backgroundColor: '#F4CE14',
- },
- innerContainer: {
-  paddingHorizontal: 40,
-  paddingVertical: 20,
-  backgroundColor: '#F4CE14',
-  flexDirection: 'column',
-  justifyContent: 'space-between',
- },
- itemText: {
-  color: '#495E57',
-  fontSize: 22,
- },
- headerText: {
-  color: '#495E57',
-  fontSize: 30,
-  textAlign: 'center',
- },
+const menuStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  innerContainer: {
+    paddingHorizontal: 40,
+    paddingVertical: 20,
+    backgroundColor: "#495E57",
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  itemText: {
+    color: "#F4CE14",
+    fontSize: 22,
+  },
+  headerText: {
+    color: "#495E57",
+    fontSize: 30,
+    textAlign: "center",
+  },
 });
